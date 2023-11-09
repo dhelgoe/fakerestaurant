5a73d320b1943f5c50a0a7438a201fffb2bf26b4
derek helgoe <derek1896@gmail.com>
2023-11-09 15:26:27 -0600

    happy with it right now, could use more design work

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    alert("Warning: This is a fictional representation and not a real bar & grill. The images, location, and content are for demonstration purposes only.");
+  };
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
